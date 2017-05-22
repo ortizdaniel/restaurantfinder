367cb9ff3ea291bda1c687c11866cbf4e1b21971
dhilazo <david.hilazo.2015@salleurl.edu>
2017-05-22 20:28:56 +0200

    Register avalible

--- app/src/main/java/pprog2/salleurl/edu/practica_pprog2/activities/MainActivity.java
+++ app/src/main/java/pprog2/salleurl/edu/practica_pprog2/activities/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     public void onRegisterButtonClick(View view){
         //TODO esperando a k dani haga su clase
         /* Llamamos a la actividad de Registro */
-        //Intent intent = new Intent(this, /*Register Activity.class*/);
-        //startActivity(intent);
+        Intent intent = new Intent(this, ActivityRegister.class);
+        startActivity(intent);
     }
 }
