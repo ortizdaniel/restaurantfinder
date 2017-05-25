67ec5f46cb2f143f1c8d11afee0716f64687cf74
dhilazo <david.hilazo.2015@salleurl.edu>
2017-05-25 16:59:39 +0200

    comments

--- app/src/main/java/pprog2/salleurl/edu/practica_pprog2/utils/DatabaseHelper.java
+++ app/src/main/java/pprog2/salleurl/edu/practica_pprog2/utils/DatabaseHelper.java
@@ -21,7 +21,7 @@ import pprog2.salleurl.edu.practica_pprog2.R;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String name = "food_db";
-    private static final int version = 6;
+    private static final int version = 7;
     private static SQLiteDatabase.CursorFactory factory;
 
     // Instancia del patron de diseño SINGLETON
